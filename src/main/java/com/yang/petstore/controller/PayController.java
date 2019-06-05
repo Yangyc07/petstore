aced3954efef30e6318bdb1198da165ae7ee5118
yang <747378285@qq.com>
2019-06-05 17:24:46 +0800

    支付宝同步回调

--- src/main/java/com/yang/petstore/controller/PayController.java
+++ src/main/java/com/yang/petstore/controller/PayController.java
@@ -2,17 +2,21 @@ package com.yang.petstore.controller;
 
 import com.alipay.api.AlipayApiException;
 import com.yang.petstore.alipay.AlipayBean;
+import com.yang.petstore.controller.ViewObject.OrderVO;
 import com.yang.petstore.error.BusinessException;
+import com.yang.petstore.response.CommonReturnType;
 import com.yang.petstore.service.OrderService;
 import com.yang.petstore.service.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @RestController("pay")
 @RequestMapping("/pay")
+@CrossOrigin(origins = {"*"},allowCredentials = "true")
 public class PayController {
 
     @Autowired
@@ -34,5 +38,19 @@ public class PayController {
                 .setOut_trade_no(out_trade_no)
                 .setTotal_amount(new StringBuffer().append(total_amount))
                 .setSubject(subject));
+    }
+
+    //同步回调
+    @RequestMapping(value = "/getReturnUrlInfo",method = RequestMethod.GET)
+    String alipayNotifyUrlInfo(HttpServletRequest request){
+        payService.synchronous(request);
+        //获取订单号
+        String out_trade_no = request.getParameter("out_trade_no");
+        //根据订单号获取订单信息
+        OrderVO orderVO = orderService.selectByOrderNo(out_trade_no);
+        request.getSession().setAttribute("orderVO",orderService.selectByOrderNo(out_trade_no));
+
+        //界面必须部署在公网上
+        return "myorder";
     }
 }
